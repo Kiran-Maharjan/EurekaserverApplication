8fe5e367b84181bbf45c4a3407521d472e94321d
Kiran-Maharjan <kiranmaharjan8888@gmail.com>
2022-10-11 00:00:43 +0545

    Microservice Spring boot Eureka Server

---
+++ EurekaserverApplication/src/test/java/com/kiran/EurekaserverApplication/EurekaserverApplication/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiran.EurekaserverApplication.EurekaserverApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
